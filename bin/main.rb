630fa9387af2a83d322349227f7c5e3b30fd25c9
KumarAmitt <amitt2015@outlook.com>
2021-01-25 14:35:27 +0530

    Create a hash of target urls

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,17 @@
+require 'httparty'
+require 'nokogiri'
+
+url = {
+  root: 'https://www.imdb.com/',
+  mv: {
+    top_rated: 'chart/top/?ref_=nv_mv_250',
+    popular: 'chart/moviemeter/?ref_=nv_mv_mpm'
+  },
+  tv: {
+    top_rated: 'chart/toptv/?ref_=nv_tvv_250',
+    popular: 'chart/tvmeter/?ref_=nv_tvv_mptv'
+  }
+}
+
+
 
